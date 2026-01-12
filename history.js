5ddbbde9bb4ff7c64bf1e501fb8d12a49717dabc
wellyyana2211060019-ux <wellyyana.2211060019@mail.darmajaya.ac.id>
2026-01-12 08:29:01 +0700

    Update history.js

--- history.js
+++ history.js
@@ -1,5 +1,5 @@
-import { getDatabase, ref, onValue, query, limitToLast } 
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase, ref, onValue, query, limitToLast }
+  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const db = getDatabase();
 
@@ -9,6 +9,25 @@ let historyChart;
 
 window.selectSensor = s => {
   selectedSensor = s;
+
+  // Update visual state
+  const map = {
+    'temperature': 'hist-temp',
+    'humidity': 'hist-humidity',
+    'gas': 'hist-gas',
+    'dust': 'hist-dust'
+  };
+
+  // Remove active form all
+  Object.values(map).forEach(id => {
+    document.getElementById(id).classList.remove('active-card');
+  });
+
+  // Add to selected
+  if (map[s]) {
+    document.getElementById(map[s]).classList.add('active-card');
+  }
+
   loadHistory();
 };
 
@@ -17,36 +36,36 @@ window.setRange = d => {
   loadHistory();
 };
 
-function loadHistory(){
+function loadHistory() {
   const ctx = document.getElementById("historyChart").getContext("2d");
-  const q = query(ref(db,"history"), limitToLast(rangeDay * 24));
+  const q = query(ref(db, "history"), limitToLast(rangeDay * 24));
 
-  onValue(q,snap=>{
-    let labels=[], data=[];
+  onValue(q, snap => {
+    let labels = [], data = [];
 
-    snap.forEach(child=>{
+    snap.forEach(child => {
       const d = child.val();
-      labels.push(new Date(d.timestamp*1000).toLocaleDateString());
+      labels.push(new Date(d.timestamp * 1000).toLocaleDateString());
       data.push(d[selectedSensor]);
     });
 
-    if(historyChart) historyChart.destroy();
+    if (historyChart) historyChart.destroy();
 
-    historyChart = new Chart(ctx,{
-      type:"line",
-      data:{
+    historyChart = new Chart(ctx, {
+      type: "line",
+      data: {
         labels,
-        datasets:[{
-          label:selectedSensor.toUpperCase(),
+        datasets: [{
+          label: selectedSensor.toUpperCase(),
           data,
-          tension:.4
+          tension: .4
         }]
       },
-      options:{
-        plugins:{
-          zoom:{
-            pan:{enabled:true,mode:"x"},
-            zoom:{wheel:{enabled:true},mode:"x"}
+      options: {
+        plugins: {
+          zoom: {
+            pan: { enabled: true, mode: "x" },
+            zoom: { wheel: { enabled: true }, mode: "x" }
           }
         }
       }
