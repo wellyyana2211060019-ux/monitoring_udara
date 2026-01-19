03cb05901245a6930bb0f9d8700b3a91a22ed7e6
NI MADE DENI ARINI <nimadedeniarini1309@gmail.com>
2026-01-19 11:25:21 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, onValue }
+import { getDatabase, ref, onValue, push }
   from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
